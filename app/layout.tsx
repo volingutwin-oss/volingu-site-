af4840d6563382739574cac5a47e859b10b7646f
Volingu Backup <backup@volingu.com>
2025-10-22 23:22:25 +0300

    Fix mobile optimization - prevent horizontal scroll, fix panels and background issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel="dns-prefetch" href="https://twin24.ai" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes, viewport-fit=cover" />
         <meta name="robots" content="noindex, nofollow" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       </head>
